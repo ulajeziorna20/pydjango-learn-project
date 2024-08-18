6b32e115012ddbaa37a4a22d82e95a9f1f7d52e2
Ula Jeziorna <jeziornaula@wp.pl>
2024-08-18 12:36:32 +0200

    Lesson 4-5 - migrationsm django admin usage, passing data to the views

--- my_project/posts/admin.py
+++ my_project/posts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+# jesli chcesz aby twoje modele pojawiły sie w panelu admin zaimportuj je tutaj i zarejestruj
+from .models import Post
+
 # Register your models here.
+
+
+admin.site.register(Post)
--- my_project/posts/views.py
+++ my_project/posts/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 def posts_list(request):
-  return render(request, 'posts/posts_list.html')
+
+# tutaj pobierasz wszystkie posty
+  posts = Post.objects.all().order_by('-date')
+
+  return render(request, 'posts/posts_list.html', {'posts': posts})
