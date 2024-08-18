a1059fd99178dfe16a6cac8cb70a3b0929724033
Ula Jeziorna <jeziornaula@wp.pl>
2024-08-18 19:10:07 +0200

    Lesson 7 - uploading pages images via admin panel

--- my_project/my_project/urls.py
+++ my_project/my_project/urls.py
@@ -22,6 +22,9 @@ from . import views
 
 from . import views
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
@@ -31,3 +34,6 @@ urlpatterns = [
     # tak dodajesz subadresy substrony
     path('posts/', include('posts.urls')),
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
