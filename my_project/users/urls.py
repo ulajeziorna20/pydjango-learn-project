42a45bb25a4dd2d6a2a1763dc0d5d2fa594360e5
Ula Jeziorna <jeziornaula@wp.pl>
2024-08-19 18:31:36 +0200

    Lesson 9 - login autentycathion

--- my_project/users/urls.py
+++ my_project/users/urls.py
@@ -6,5 +6,6 @@ app_name = 'users'
 
 urlpatterns = [
     path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
     # slug left path converter, right received paramits nly name
 ]
--- my_project/users/views.py
+++ my_project/users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-# rejestracja
-from django.contrib.auth.forms import UserCreationForm 
-
+# rejestracja 
+# login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 # Create your views here.
 
@@ -11,7 +12,7 @@ def register_view(request):
     form = UserCreationForm(request.POST)
 
     if form.is_valid():
-      form.save()
+      login(request, form.save())
       return redirect("posts:list")
     else:
       print(form.errors)  # Wydrukuj błędy w konsoli
@@ -22,4 +23,26 @@ def register_view(request):
     form = UserCreationForm()
 
     return render(request, 'register.html', { "form": form })
+
+
+def login_view(request):
+
+  if request.method == "POST":
+    form = AuthenticationForm(data=request.POST)
+
+    if form.is_valid():
+        
+      # login here needs a request and user value get by this method
+      login(request, form.get_user())
+      return redirect("posts:list")
+    
+    else:
+      print(form.errors)  # Wydrukuj błędy w konsoli
+      # Jeśli formularz nie jest poprawny, renderuj ten sam formularz z błędami
+      return render(request, 'login.html', { "form": form })
+
+  else:
+    form = AuthenticationForm()
+
+    return render(request, 'login.html', { "form": form })
 
